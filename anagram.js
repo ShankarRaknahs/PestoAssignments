8289ff3eab077d96be112c220c8c6c2c7f64f8de
Shankar Ganesan <shankar@Shankars-MacBook-Pro.local>
2020-12-06 21:27:33 +0530

    find the count of anagram in the given phrase

---
+++ anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @function - To find the valid number of anagrams in it
+ * @param - any string/ phrase 
+ * @returns - count of anagram
+ */
+
+function findAnagramCount(phrase) {
+    let uniqueWords = [];
+    let anagrams = [];
+
+    phrase.split(/[ \(,\.?)]+/).forEach((word) => {
+        let wordSorted = word.trim().toLowerCase().split('').sort().join('');
+        if (wordSorted && wordSorted.length > 1 && !uniqueWords.includes(wordSorted)) {
+            uniqueWords.push(wordSorted);
+        } else if (wordSorted && wordSorted.length > 1 && uniqueWords.includes(wordSorted) && !anagrams.includes(wordSorted)) {
+            anagrams.push(wordSorted);
+        }
+    });
+    return anagrams.length;
+
+}
+
+console.log(findAnagramCount('aa aa odg dog gdo')); // 2
+console.log(findAnagramCount('a c b c run urn urn')); // 1
+console.log(findAnagramCount('k k k k bar foo')); // 0
